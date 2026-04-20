7746a32b83c6bd36c9ea628817f82cea8821fa9b
Israel Osunbor <speedrhythmics@gmail.com>
2026-04-20 12:31:59 +0100

    Adding SpriteRenderer to the rendering system.

---
+++ ParsnipEngine/Interfaces/IComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsnipEngine.Interfaces
+{
+    /// <summary>
+    /// Interfaced enforced by entity components.
+    /// </summary>
+    public interface IComponent
+    {
+
+    }
+}
---
+++ ParsnipEngine/Rendering/Components/SpriteRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ParsnipEngine.Rendering.Components
+{
+    /// <summary>
+    /// A renderer specialised for drawing singular sprites. <br/>
+    /// 
+    /// NOTE: This should be used within entity objects later in development!
+    /// </summary>
+    public class SpriteRenderer
+    {
+        /// <summary>
+        /// The sprite used by this renderer component as the texture during the drawing phase.
+        /// </summary>
+        public Texture2D Sprite { get; private set; }
+
+        /// <summary>
+        /// Shading color applied to this renderer.
+        /// </summary>
+        public Color ShaderColor { get; private set; } = Color.White;
+
+        /// <summary>
+        /// Draws this renderer's sprite.
+        /// </summary>
+        /// <param name="spriteBatch">Batch used to draw this renderer's sprite.</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            float scaleX = Camera.Main.PixelResolutionScaleFactor(Sprite.Width);
+            float scaleY = Camera.Main.PixelResolutionScaleFactor(Sprite.Height);
+            
+            Vector2 scale = new Vector2(scaleX, scaleY);
+            Vector2 position = Camera.Main.WorldToScreenPosition(Vector2.Zero);
+
+            spriteBatch.Draw(Sprite, position, null, ShaderColor, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        }
+    }
+}
